aa14ae34e3e2d1e01c85b5a1dc47ca597e6d77e6
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-19 15:46:23 +0100

    Add If/Else If & Stamp Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,36 @@
 */
 
 //Variabili Chilometri / Età
-let km = prompt(`Quanti km devi percorrere ?`)
-const age = prompt(`Qual'è la tua età ?`)
+let km = prompt(`Quanti km devi percorrere ?`);
+km = Number(km);
+let age = prompt(`Qual'è la tua età ?`);
+age = Number(age);
+
+//Variabile Costo Biglietto
+var ticketPrice = (km * 0.21);
+ticketPrice = Number(ticketPrice)
+//Sconto minorenni e anziani
+if (age < 18) { //SE meno di 18 anni
+    const minorDiscount = ((ticketPrice / 100) * 20);
+    ticketPrice = (ticketPrice - minorDiscount);
+} else if (age > 65) { //SE piu di 65 anni
+    const oldDiscount = ((ticketPrice / 100) * 40);
+    ticketPrice = (ticketPrice - oldDiscount);
+}
+
+//STAMP
+console.log(`Il Prezzo del tuo biglietto è di ${ticketPrice.toFixed(2)}€`)
+
+
+//If i want stamp on If and Else If
+/*
+console.log(`Il prezzo per un minorenne è di ${ ticketPrice }€`)
+
+console.log(`Il prezzo per un anziano è di ${ ticketPrice }€`)
+*/
+
+
+
 
 
 
