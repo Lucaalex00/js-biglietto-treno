9ee998ddfbad90e76c38ad7689efbda71f6e1d62
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-02-19 15:46:41 +0100

    Add Personal Bonus Commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,6 +36,32 @@ console.log(`Il prezzo per un minorenne è di ${ ticketPrice }€`)
 console.log(`Il prezzo per un anziano è di ${ ticketPrice }€`)
 */
 
+// BONUS
+
+//Variabile Costo Biglietto RANDOM
+
+let r_km = Math.floor(Math.random() * 1000 + 1)
+let r_age = Math.floor(Math.random() * 100 + 1)
+var r_ticketPrice = (r_km * 0.21);
+ticketPrice = Number(r_ticketPrice)
+
+document.writeln("Random Kms " + r_km + "<hr>") //Stamp with HR tag
+document.writeln("Random Age " + r_age)
+//Sconto minorenni e anziani RANDOM
+if (r_age < 18) { //SE meno di 18 anni
+    const minorDiscount = ((r_ticketPrice / 100) * 20);
+    r_ticketPrice = (r_ticketPrice - minorDiscount);
+    console.log(`Il prezzo generato randomicamente ad un minorenne è di ${r_ticketPrice.toFixed(2)}€`)
+} else if (r_age > 65) { //SE piu di 65 anni
+    const oldDiscount = ((r_ticketPrice / 100) * 40);
+    r_ticketPrice = (r_ticketPrice - oldDiscount);
+    console.log(`Il prezzo generato randomicamente ad un anziano è di ${r_ticketPrice.toFixed(2)}€`)
+} else { //STAMP RANDOM Price
+    console.log(`Il Prezzo del tuo biglietto, generato randomicamente, è di ${r_ticketPrice.toFixed(2)}€`)
+}
+
+
+
 
 
 
